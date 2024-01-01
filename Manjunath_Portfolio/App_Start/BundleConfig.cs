65dab9e26ee9d96a3a5ceef13961f5b698563f47
savit <savit@LAPTOP-2L38R8JI>
2024-01-01 13:04:27 +0530

    Add project files.

---
+++ Manjunath_Portfolio/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+namespace Manjunath_Portfolio
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-3.4.1.min.js"));
+
+            //bootstrap 
+            bundles.Add(new StyleBundle("~/bundles/bootstrapstyles").Include(
+                "~/Content/Bootstrap5/css/bootstrap.min.css"
+                ));
+
+            bundles.Add(new Bundle("~/bundles/bootstrapjs").Include(
+                "~/Content/Bootstrap5/js/bootstrap.min.js"
+                ));
+
+            //fontawesome
+            bundles.Add(new StyleBundle("~/bundles/fontawesome").Include(
+                "~/content/font-awesome.min.css"
+                ));
+
+            //css : custom css
+            bundles.Add(new Bundle("~/bundles/customcss").Include(
+                   "~/Content/CustomCSS/customcss1.css"
+                   ));
+        }
+    }
+}
---
+++ Manjunath_Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using System.Web.UI.WebControls;
+
+namespace Manjunath_Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendMail(mailobject emailset)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                //mail.To.Add("rohitgaikwad853@gmail.com");
+                mail.To.Add(emailset.Email);
+                mail.From = new MailAddress("savitrigolmanjunath@gmail.com");
+                mail.Subject = emailset.Subject;
+                mail.Body = emailset.Message;
+                mail.IsBodyHtml = false;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = Convert.ToInt32(587);
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("savitrigolmanjunath@gmail.com", "qnru yszf mvqh jsye"); // Enter senders User name and password  
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+            }
+            catch
+            {
+
+            }
+            return View("Index");
+        }
+
+
+    }
+
+    public class mailobject
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
